53b19e4f10adf16437d5adcec98a10788d2fd8fb
ChinmayaBisoi <bisoi.chinmaya.1999@gmail.com>
2023-11-21 23:42:38 +0530

    auto transition working

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -11,6 +11,8 @@ function CarouselItem({ imgUrl, imgTitle }) {
 const Carousel = ({ autoScroll = false }) => {
   const items = ["Item 1", "Item 2", "Item 3", "Item 4"]; // Replace with your carousel items
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [transition, setTransition] = useState(false);
+  const [cards, setCards] = useState(items);
 
   const totalItems = items.length;
 
@@ -18,27 +20,46 @@ const Carousel = ({ autoScroll = false }) => {
     const intervalId = setInterval(() => {
       // Move to the next item
       setCurrentIndex((prevIndex) => (prevIndex + 1) % totalItems);
+      setTransition(true);
     }, 3000); // Adjust the interval duration as needed
 
     return () => clearInterval(intervalId);
   }, [totalItems]);
 
+  function handleTransitionEnd() {
+    console.log("transition ended");
+    const newCards = [
+      ...items.slice(currentIndex),
+      ...items.slice(0, currentIndex),
+    ];
+
+    setCards(newCards);
+    setTransition(false);
+  }
+
+  useEffect(() => {
+    console.log(transition);
+  });
+
   return (
-    <div
-      className="text-white overflow-hidden mb-2 relative flex gap-4"
-      id="container"
-    >
-      {items.map((item) => {
-        return (
-          <div
-            key={item}
-            style={{ transform: `translateX(-${currentIndex * 216}px)` }}
-            className={`w-[200px] min-w-[200px] bg-red-400 h-[200px] transform duration-500 ease-in-out ${""}`}
-          >
-            {item}
-          </div>
-        );
-      })}
+    <div className="text-white overflow-hidden mb-2 relative" id="container">
+      <div
+        onTransitionEnd={handleTransitionEnd}
+        className={`flex gap-4 transform duration-500 ease-in-out ${
+          transition ? "translate-x-[-216px]" : "transition-none"
+        }  `}
+      >
+        {cards.map((item) => {
+          return (
+            <div
+              key={item}
+              className={`w-[200px] min-w-[200px] bg-red-400 h-[200px]`}
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
